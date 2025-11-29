c3737c0ebb3146726a5d8835afd713da632f814c
mansingh-04 <manwork.tr@gmail.com>
2025-11-29 18:23:22 +0530

    final touch

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ app.use('/users', usersRouter);
 const path = require('path');
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+// app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
 // Anything that doesn't match the above, send back index.html
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
-});
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+// });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
