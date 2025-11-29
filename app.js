ba0d917c93b5377e6761d0860e9be9f5c7d360a3
mansingh-04 <manwork.tr@gmail.com>
2025-11-29 16:16:05 +0530

    try

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
 });
 const PORT = process.env.PORT || 3000;
